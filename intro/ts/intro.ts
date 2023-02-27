60336fc0f2f905cbddd0f1d4319cb7d89d890e25
Jean Rondon <jeanrdev@gmail.com>
2023-02-27 04:09:29 -0400

    intro - Clases y objetos en TypeScript

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -1,1 +1,18 @@
-console.log('holaa desde ts');
+// console.log('holaa desde ts');
+
+class Drink {
+
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  info(): string {
+    return this.name;
+  }
+
+}
+
+const drink = new Drink('agua');
+console.log(drink.info());
