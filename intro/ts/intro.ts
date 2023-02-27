ff77ecdd82a35e152f9825482ca08bca98e12d57
Jean Rondon <jeanrdev@gmail.com>
2023-02-27 04:47:43 -0400

    intro - Interfaces en TypeScript

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -14,27 +14,72 @@ class Drinks {
 
 }
 
-const drinka = new Drink('agua');
-console.log(drinka.info());
+const drinks = new Drinks('agua');
+console.log(drinks.info());
+
+// interfaces en ts
+interface Product {
+  price: number;
+  getPrice(): string;
+}
 
 // herencia en ts
-class Beers extends Drinks {
+class Beers extends Drinks implements Product {
 
   private alcohol: number;
+  price: number;
 
-  constructor(name: string, alcohol: number) {
+  constructor(name: string, alcohol: number, price: number) {
 
     super(name);
 
     this.alcohol = alcohol;
+    this.price = price;
 
   }
 
+  getPrice(): string {
+    return 'El precio de ' + super.info() + ' es: $ ' + this.price;
+  }
+
   info(): string {
-    return super.info() + ' ' + this.alcohol;
+    return 'El precio de ' + super.info() + ' es ' + this.alcohol;
   }
 
 }
 
-const beers = new Beers('Vodka', 8.5);
-console.log(beers.info());
+// implementación de interface
+class Snack implements Product {
+
+  name: string;
+  price: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+
+  getPrice(): string {
+    return 'El precio del producto ' + this.name + ' es: $ ' + this.price;
+  }
+
+}
+
+// const beers = new Beers('Vodka', 8.5, 120);
+// console.log(beers.info());
+
+const products: Product[] = [
+  new Beers('Tequila', 8.5, 75),
+  new Snack('Dorito', 10),
+  new Beers('Polar', 5.5, 25),
+]
+
+function getPrices(items: Product[]) {
+
+  for (const item of items) {
+    console.log(item.getPrice());
+  }
+
+}
+
+getPrices(products);
