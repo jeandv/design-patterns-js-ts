4a5ac844a6dc6b74b08482990e960b061d8fd999
Jean Rondon <jeanrdev@gmail.com>
2023-02-26 23:02:45 -0400

    intro - funciones de primer orden y orden superior

--- intro/js/intro.js
+++ intro/js/intro.js
@@ -1,1 +1,20 @@
-console.log('hola');
+// console.log('hola');
+
+function sum(a, b) {
+  return a + b;
+}
+
+let res = sum(1, 2);
+console.log(res);
+
+const fSum = sum;
+res = fSum(5, 6);
+console.log(res);
+
+// función orden superior
+function operation(fn, a, b) {
+  console.log('se hace algo');
+  console.log(fn(a, b));
+}
+
+operation(sum, 10, 20);
