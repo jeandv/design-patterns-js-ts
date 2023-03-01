993bf02f609712058103fa96fb05852af6cc3a97
Jean Rondon <jeanrdev@gmail.com>
2023-03-01 00:24:35 -0400

    Singleton - Singleton en JavaScript - caso práctico

--- singleton/singleton.js
+++ singleton/singleton.js
@@ -22,9 +22,40 @@ const singleton = new Singleton();
 const singleton2 = new Singleton();
 const singleton3 = Singleton.getInstance();
 
-console.log(singleton.random);
-console.log(singleton2.random);
-console.log(singleton3.random);
+// console.log(singleton.random);
+// console.log(singleton2.random);
+// console.log(singleton3.random);
 
-console.log(singleton === singleton2);
-console.log(singleton2 === singleton3);
+// console.log(singleton === singleton2);
+// console.log(singleton2 === singleton3);
+
+class WeekDays {
+
+  daysEs = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+  daysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  constructor(lang) {
+    this.lang = lang;
+
+    if (WeekDays.instance) {
+      return WeekDays.instance;
+    }
+
+    WeekDays.instance = this;
+
+  }
+
+  getDays() {
+    return this.lang === 'es' ?
+      this.daysEs :
+      this.daysEn
+  }
+
+}
+
+const weekDays = new WeekDays('en');
+console.log(weekDays.getDays());
+
+const weekDays2 = new WeekDays('es');
+console.log(weekDays2.getDays());
