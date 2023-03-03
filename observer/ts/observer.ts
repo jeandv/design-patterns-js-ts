ebd097fb2629b3ee2f71436a342b8ab8e90a3d0c
Jean Rondon <jeanrdev@gmail.com>
2023-03-03 06:06:57 -0400

    Observer en TypeScript

--- observer/ts/observer.ts
+++ observer/ts/observer.ts
@@ -1,1 +1,72 @@
 console.log('Patron de diseño OBSERVER!! con TS.');
+
+interface IObserver<T> {
+  refresh(value: T): void;
+}
+
+interface ISubject<T> {
+  observers: IObserver<T>[];
+  subscribe(observer: IObserver<T>): void;
+  unsubscribe(observer: IObserver<T>): void;
+  notify(value: T): void;
+}
+
+class Subject<T> implements ISubject<T> {
+  observers: IObserver<T>[];
+
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(observer: IObserver<T>): void {
+
+    this.observers.push(observer);
+
+  }
+
+  unsubscribe(observer: IObserver<T>): void {
+
+    this.observers.filter(obs => obs !== observer);
+
+  }
+
+
+  notify(value: T): void {
+
+    this.observers.forEach(e => e.refresh(value));
+
+  }
+
+}
+
+class Observer<T> implements IObserver<T> {
+
+  private fn: (value: T) => void;
+
+  constructor(fn: (value: T) => void) {
+    this.fn = fn;
+  }
+
+  refresh(value: T): void {
+    this.fn(value);
+  }
+
+}
+
+const subject = new Subject<number>();
+
+const obs1 = new Observer<number>((n) => console.log('este es el nro: ' + n));
+
+const obs2 = new Observer<number>((n) => console.log('este es el nro: ' + n));
+
+subject.subscribe(obs1);
+subject.subscribe(obs2);
+
+subject.notify(10);
+subject.notify(2003);
+
+const subject2 = new Subject<string>();
+const obs3 = new Observer<string>((name) => console.log('Hola: ' + name.toLowerCase()));
+
+subject2.subscribe(obs3);
+subject2.notify('Jean Rondón');
