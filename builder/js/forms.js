08d03e7bf5affb18993d0066661f549000d01955
Jean Rondon <jeanrdev@gmail.com>
2023-04-04 01:52:34 -0400

    Builder en JavaScript - caso practico

---
+++ builder/js/forms.js
@@ -0,0 +1,119 @@
+class Form {
+
+  constructor(controls, action) {
+    this.controls = controls;
+    this.action = action;
+  }
+
+  getContent() {
+    return (
+      `
+      <form method="post" action="${this.action}">
+      ${this.controls.reduce((acc, c) => {
+
+        return acc + `<div>${this.getLabel(c)} ${this.getInput(c)}</div>`
+
+      }, "")}
+
+      <button type="submit">Enviar</button>
+
+      </form>
+      `
+    )
+  }
+
+  getLabel(control) {
+    return `<label>${control.text}</label>`
+  }
+
+  getInput(control) {
+    return `<input type="${control.type}" id="${control.name}" name="${control.name}"/>`
+  }
+
+}
+
+class FormBuilder {
+
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.action = '';
+    this.controls = [];
+  }
+
+  setAction(action) {
+    this.action = action;
+    return this;
+  }
+
+  setText(name, text) {
+    this.controls.push({
+      name: name,
+      text: text,
+      type: 'text'
+    });
+
+    return this;
+  }
+
+  setEmail(name, text) {
+    this.controls.push({
+      name: name,
+      text: text,
+      type: 'email'
+    });
+
+    return this;
+  }
+
+  setCheckBox(name, text) {
+    this.controls.push({
+      name: name,
+      text: text,
+      type: 'checkbox'
+    });
+
+    return this;
+  }
+
+  setColor(name, text) {
+    this.controls.push({
+      name: name,
+      text: text,
+      type: 'color'
+    });
+
+    return this;
+  }
+
+  build() {
+    const frm = new Form(this.controls, this.action);
+
+    this.reset();
+
+    return frm;
+  }
+
+}
+
+const formBuilder = new FormBuilder();
+
+const formPeople = formBuilder.setAction('add.php')
+  .setText('firstName', 'Nombre')
+  .setText('lastName', 'Apellido')
+  .setCheckBox('edad', 'Eres mayor de edad?')
+  .setColor('favColor', 'Escoge tu color favorito')
+  .build();
+
+console.log(formPeople);
+
+form1.innerHTML = formPeople.getContent();
+
+const formMail = formBuilder.setAction('send.php')
+  .setText('firstName', 'Nombre')
+  .setEmail('email', 'Correo')
+  .build();
+
+form2.innerHTML = formMail.getContent();
